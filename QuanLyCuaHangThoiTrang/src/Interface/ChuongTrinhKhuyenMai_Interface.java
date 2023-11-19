d8195e4678493326bf00b28588853297b2edbbb5
DinhNguyenChung <127194159+DinhNguyenChung@users.noreply.github.com>
2023-11-19 14:04:25 +0700

    Update ChuongTrinhKhuyenMai_Interface.java

--- QuanLyCuaHangThoiTrang/src/Interface/ChuongTrinhKhuyenMai_Interface.java
+++ QuanLyCuaHangThoiTrang/src/Interface/ChuongTrinhKhuyenMai_Interface.java
@@ -16,10 +16,8 @@ public interface ChuongTrinhKhuyenMai_Interface {
     public boolean delete (ChuongTrinhKhuyenMaiEntity ctkm);
     public boolean update (ChuongTrinhKhuyenMaiEntity ctkm);
     public ArrayList<LoaiKhuyenMaiEntity> getallLoaiCTKM() ;
-<<<<<<< HEAD
     public ArrayList<ChuongTrinhKhuyenMaiEntity> getallCTKMtheoLoaiKM(String ma);
-=======
->>>>>>> 3889e5a70f3db062bc1350b0b31bb7314a235503
+
     // Nguyen Huy Hoang
     public ChuongTrinhKhuyenMaiEntity kiemTraKhuyenMai(double tongTien);
 }
